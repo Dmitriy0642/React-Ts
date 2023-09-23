326a72aba48752dbb42d034b56f00e29f8cecac8
Dmitriy0642 <dmitriy.linnik1337@mail.ru>
2023-09-23 22:05:34 +0200

    end-project

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useState } from "react";
+import InputField from "./componenst/InputField";
+import "./App.css";
+import { Todo } from "./model";
+import TodoList from "./componenst/TodoList";
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+};
+
+export default App;
